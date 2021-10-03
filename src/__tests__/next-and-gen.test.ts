790758f660dd766d2e3be648297c8d13896e403f
hasany <yilmazhasan111@gmail.com>
2021-10-03 15:53:35 +0300

    Add next and gen test

---
+++ src/__tests__/next-and-gen.test.ts
@@ -0,0 +1,31 @@
+import CustomPermutation from "../../lib/CustomPermutation";
+
+test('next', () => {
+    let customPerm = new CustomPermutation(['a', 'b', 'c'], {}, {});
+    let count = 0;
+    while (true) {
+        let next = customPerm.next();
+        if (!next) {
+            break;
+        }
+        count++;
+    }
+    expect(count).toBe(6);
+})
+
+
+test('Generator', () => {
+    let customPerm = new CustomPermutation(['a', 'b', 'c'], {}, {});
+    let count = 0;
+    let gen = null;
+    gen = customPerm.generator();
+    while (true) {
+        let next = gen.next();
+        if (next.done) {
+            break;
+        }
+        count++;
+    }
+
+    expect(count).toBe(6);
+})
