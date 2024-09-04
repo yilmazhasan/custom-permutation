37d62701d51cf0dadd441d10ac77d8a115561547
yilmazhasan <yilmazhasan111@gmail.com>
2024-09-04 18:43:46 +0300

    Rename tests and add edge cases tests

---
+++ src/__tests__/edge-cases.test.ts
@@ -0,0 +1,63 @@
+import CustomPermutation from "../../lib/CustomPermutation";
+
+test('Empty list with next', () => {
+    let customPerm = new CustomPermutation([], {}, {});
+    let count = 0;
+    while (true) {
+        let next = customPerm.next();
+        if (!next) {
+            break;
+        }
+        count++;
+    }
+    expect(count).toBe(0);
+})
+
+test('Empty list with generator', () => {
+    let customPerm = new CustomPermutation([], {}, {});
+    let count = 0;
+    let gen: any = null;
+    gen = customPerm.generator();
+    while (true) {
+        let next = gen.next();
+        if (next.done) {
+            break;
+        }
+        count++;
+    }
+
+    expect(count).toBe(0);
+});
+
+test('One element list with next', () => {
+    let customPerm = new CustomPermutation(['a'], {}, {});
+    let count = 0;
+    while (true) {
+        let next = customPerm.next();
+        if (!next) {
+            break;
+        }
+        count++;
+    }
+    expect(count).toBe(1);
+})
+
+
+test('One element list with generator', () => {
+    let customPerm = new CustomPermutation(['a'], {}, {});
+    let count = 0;
+    let gen: any = null;
+    gen = customPerm.generator();
+    while (true) {
+        let next = gen.next();
+        if (next.done) {
+            break;
+        }
+        count++;
+    }
+
+    expect(count).toBe(1);
+})
+
+
+
